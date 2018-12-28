c0fbcb5e658a73fa4323fa04faf105f3229f2878
cozzyx <44362176+cozzyx@users.noreply.github.com>
2018-12-28 15:02:35 +0000

    adjusted size for desktop homepage

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,4 @@
 console.log("hello");
-/*
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-*/
 
 const menuToggle = document.getElementById("menu-toggle");
 const menuNav = document.getElementById("menu-nav");
